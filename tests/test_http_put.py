c875d7fffcd0abd482433237deab78be48047891
mac <ihimself@gmail.com>
2021-02-16 00:41:23 +0300

    added more tests to PUT and fixed copy-paste naming

--- tests/test_http_put.py
+++ tests/test_http_put.py
@@ -9,7 +9,7 @@ from conftest import INVALID_BEAR_NAMES, INVALID_BEAR_AGES, INVALID_BEAR_TYPES
 
 
 #
-#  Assist function for valid data verification.
+#  Assist function for valid data verification (part).
 # 
 #  Gets valid_bear dict and changes one of it's specified params to specified value.
 #  Creates a bear, updates one of bear specified params to a specified value
@@ -49,7 +49,7 @@ def test_update_bear_age_by_sending_only_that_valid_param(valid_bear, valid_bear
 
 
 #
-#  Assist function for invalid data verification.
+#  Assist function for invalid data verification (part).
 # 
 #  Gets valid_bear dict and changes one of it's specified params to specified value.
 #  Creates a bear, tries to update one of bear specified params to a specified value
@@ -66,24 +66,56 @@ def post_put_part_and_get_400(valid_bear, param, value):
 
 # @pytest.mark.d
 @pytest.mark.parametrize("invalid_bear_name", INVALID_BEAR_NAMES)
-def test_update_bear_name_by_sending_only_that_valid_param(valid_bear, invalid_bear_name):
+def test_update_bear_name_by_sending_only_that_invalid_param(valid_bear, invalid_bear_name):
   post_put_part_and_get_400(valid_bear, "bear_name", invalid_bear_name)
 
 
 # @pytest.mark.d
 @pytest.mark.parametrize("invalid_bear_type", INVALID_BEAR_TYPES)
-def test_update_bear_type_by_sending_only_that_valid_param(valid_bear, invalid_bear_type):
+def test_update_bear_type_by_sending_only_that_invalid_param(valid_bear, invalid_bear_type):
   post_put_part_and_get_400(valid_bear, "bear_type", invalid_bear_type)
 
 
 # @pytest.mark.d
 @pytest.mark.parametrize("invalid_bear_age", INVALID_BEAR_AGES)
-def test_update_bear_age_by_sending_only_that_valid_param(valid_bear, invalid_bear_age):
+def test_update_bear_age_by_sending_only_that_invalid_param(valid_bear, invalid_bear_age):
   post_put_part_and_get_400(valid_bear, "bear_age", invalid_bear_age)
 
 
+# @pytest.mark.d
+def test_update_bear_unknown_parameter_sending_only_that_invalid_param(valid_bear):
+  post_put_part_and_get_400(valid_bear, "unknown", "parameter")
+
+
+# @pytest.mark.d
+def test_update_bear_id_to_new_free_id_sending_only_that_param(valid_bear):
+  r1 = requests.post(SERVICE_URL, data=json.dumps(valid_bear), timeout=T)
+  assert r1.status_code == 200
+  bear_id1 = r1.text
+  r2 = requests.post(SERVICE_URL, data=json.dumps(valid_bear), timeout=T)
+  assert r2.status_code == 200
+  bear_id2 = r2.text
+  r3 = requests.delete(SERVICE_URL + "/" + str(bear_id1), timeout=T)  # make bear_id1 free
+  assert r3.status_code == 200
+  param = {"bear_id": bear_id1}
+  r4 = requests.put(SERVICE_URL + "/" + str(bear_id2), data=json.dumps(param), timeout=T)
+  assert r2.status_code == 400
+
+
+# @pytest.mark.d
+def test_update_bear_id_to_new_already_used_id_sending_only_that_param(valid_bear):
+  r1 = requests.post(SERVICE_URL, data=json.dumps(valid_bear), timeout=T)
+  assert r1.status_code == 200
+  bear_id1 = r1.text
+  r2 = requests.post(SERVICE_URL, data=json.dumps(valid_bear), timeout=T)
+  assert r2.status_code == 200
+  bear_id2 = r2.text
+  param = {"bear_id": bear_id1}
+  r4 = requests.put(SERVICE_URL + "/" + str(bear_id2), data=json.dumps(param), timeout=T)
+  assert r2.status_code == 400
+
 #
-#  Assist function for valid data verification.
+#  Assist function for valid data verification (whole).
 # 
 #  Gets valid_bear dict and changes one of it's specified params to specified value.
 #  Creates a bear, updates one of bear specified params to a specified value
@@ -106,24 +138,24 @@ def post_put_whole_get_and_compare(valid_bear, param, value):
 
 # @pytest.mark.d
 @pytest.mark.parametrize("valid_bear_name", VALID_BEAR_NAMES)
-def test_update_bear_name_by_sending_only_that_valid_param(valid_bear, valid_bear_name):
+def test_update_bear_name_by_sending_whole_valid_updated_bear(valid_bear, valid_bear_name):
   post_put_whole_get_and_compare(valid_bear, "bear_name", valid_bear_name)
 
 
 # @pytest.mark.d
 @pytest.mark.parametrize("valid_bear_type", VALID_BEAR_TYPES)
-def test_update_bear_type_by_sending_only_that_valid_param(valid_bear, valid_bear_type):
+def test_update_bear_type_by_sending_whole_valid_updated_bear(valid_bear, valid_bear_type):
   post_put_whole_get_and_compare(valid_bear, "bear_type", valid_bear_type)
 
 
 # @pytest.mark.d
 @pytest.mark.parametrize("valid_bear_age", VALID_BEAR_AGES)
-def test_update_bear_age_by_sending_only_that_valid_param(valid_bear, valid_bear_age):
+def test_update_bear_age_by_sending_whole_valid_updated_bear(valid_bear, valid_bear_age):
   post_put_whole_get_and_compare(valid_bear, "bear_age", valid_bear_age)
 
 
 #
-#  Assist function for invalid data verification.
+#  Assist function for invalid data verification (whole).
 # 
 #  Gets valid_bear dict and changes one of it's specified params to specified value.
 #  Creates a bear, tries to update one of bear specified params to a specified value
@@ -138,23 +170,24 @@ def post_put_whole_and_get_400(valid_bear, param, value):
   assert r2.status_code == 400  # must be HTTP 400 Bad Request
 
 
-@pytest.mark.d
+# @pytest.mark.d
 @pytest.mark.parametrize("invalid_bear_name", INVALID_BEAR_NAMES)
-def test_update_bear_name_by_sending_only_that_valid_param(valid_bear, invalid_bear_name):
+def test_update_bear_name_by_sending_whole_invalid_updated_bear(valid_bear, invalid_bear_name):
   post_put_whole_and_get_400(valid_bear, "bear_name", invalid_bear_name)
 
 
 # @pytest.mark.d
 @pytest.mark.parametrize("invalid_bear_type", INVALID_BEAR_TYPES)
-def test_update_bear_type_by_sending_only_that_valid_param(valid_bear, invalid_bear_type):
+def test_update_bear_type_by_sending_whole_invalid_updated_bear(valid_bear, invalid_bear_type):
   post_put_whole_and_get_400(valid_bear, "bear_type", invalid_bear_type)
 
 
 # @pytest.mark.d
 @pytest.mark.parametrize("invalid_bear_age", INVALID_BEAR_AGES)
-def test_update_bear_age_by_sending_only_that_valid_param(valid_bear, invalid_bear_age):
+def test_update_bear_age_by_sending_whole_invalid_updated_bear(valid_bear, invalid_bear_age):
   post_put_whole_and_get_400(valid_bear, "bear_age", invalid_bear_age)
 
+###
 
 """
 update only 1 param by sending 1 param (for each param) (with valid/invalid data)
